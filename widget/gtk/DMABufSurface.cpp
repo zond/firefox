21637c09d47d67ceb60d2d2cacb3720492356d79
Martin Stransky <stransky@redhat.com>
2025-07-11 07:24:07 +0000

    Bug 1976755 [Linux] Don't explicitly release DMABufSurface from FFmpegVideoFramePool destructor r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D256871

--- widget/gtk/DMABufSurface.cpp
+++ widget/gtk/DMABufSurface.cpp
@@ -376,9 +376,11 @@ DMABufSurface::DMABufSurface(SurfaceType aSurfaceType)
       mPID(0),
       mCanRecycle(true),
       mSurfaceLock("DMABufSurface") {
+  MOZ_COUNT_CTOR(DMABufSurface);
 }
 
 DMABufSurface::~DMABufSurface() {
+  MOZ_COUNT_DTOR(DMABufSurface);
   FenceDelete();
   GlobalRefRelease();
   GlobalRefCountDelete();
@@ -639,7 +641,10 @@ DMABufSurfaceRGBA::DMABufSurfaceRGBA()
       mTexture(0),
       mBufferModifier(DRM_FORMAT_MOD_INVALID) {}
 
-DMABufSurfaceRGBA::~DMABufSurfaceRGBA() { ReleaseSurface(); }
+DMABufSurfaceRGBA::~DMABufSurfaceRGBA() {
+  LOGDMABUF("DMABufSurfaceRGBA::~DMABufSurfaceRGBA() UID %d", mUID);
+  ReleaseSurface();
+}
 
 bool DMABufSurfaceRGBA::OpenFileDescriptorForPlane(
     DMABufDeviceLock* aDeviceLock, int aPlane) {
@@ -1130,6 +1135,8 @@ void DMABufSurfaceRGBA::ReleaseTextures() {
 }
 
 void DMABufSurfaceRGBA::ReleaseSurface() {
+  LOGDMABUF("DMABufSurfaceRGBA::ReleaseSurface() UID %d", mUID);
+
   MOZ_ASSERT(!IsMapped(), "We can't release mapped buffer!");
 
   ReleaseTextures();
@@ -1438,7 +1445,10 @@ DMABufSurfaceYUV::DMABufSurfaceYUV()
   }
 }
 
-DMABufSurfaceYUV::~DMABufSurfaceYUV() { ReleaseSurface(); }
+DMABufSurfaceYUV::~DMABufSurfaceYUV() {
+  LOGDMABUF("DMABufSurfaceYUV::~DMABufSurfaceYUV() UID %d", mUID);
+  ReleaseSurface();
+}
 
 bool DMABufSurfaceYUV::OpenFileDescriptorForPlane(DMABufDeviceLock* aDeviceLock,
                                                   int aPlane) {
