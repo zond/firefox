58f8e1ab62b292e547e6d83f96c372ab48071342
André Bargull <andre.bargull@gmail.com>
2025-07-11 15:04:26 +0000

    Bug 1972489 - Part 9: Cache duration unit options, fractional digits, and time separators. r=spidermonkey-reviewers,dminor
    This avoids calling `intl::GetInternalsObject` and `GetTimeSeparator` repeatedly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D253933

--- js/src/builtin/intl/DurationFormat.h
+++ js/src/builtin/intl/DurationFormat.h
@@ -21,6 +21,47 @@ class NumberFormat;
 
 namespace js {
 
+namespace intl {
+enum class DurationDisplay : uint8_t { Auto, Always };
+enum class DurationStyle : uint8_t { Long, Short, Narrow, Numeric, TwoDigit };
+
+struct DurationFormatOptions {
+  // Packed representation to keep the unit options as small as possible.
+  //
+  // Use |uint8_t| instead of the actual enum type to avoid GCC warnings:
+  // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=61414
+#define DECLARE_DURATION_UNIT(name)                \
+  /* DurationDisplay */ uint8_t name##Display : 1; \
+  /* DurationStyle */ uint8_t name##Style : 3;
+
+  DECLARE_DURATION_UNIT(years);
+  DECLARE_DURATION_UNIT(months);
+  DECLARE_DURATION_UNIT(weeks);
+  DECLARE_DURATION_UNIT(days);
+  DECLARE_DURATION_UNIT(hours);
+  DECLARE_DURATION_UNIT(minutes);
+  DECLARE_DURATION_UNIT(seconds);
+  DECLARE_DURATION_UNIT(milliseconds);
+  DECLARE_DURATION_UNIT(microseconds);
+  DECLARE_DURATION_UNIT(nanoseconds);
+
+#undef DECLARE_DURATION_UNIT
+
+  int8_t fractionalDigits;
+};
+
+struct DurationUnitOptions {
+  // Use the same bit-widths for fast extraction from DurationFormatOptions.
+  /* DurationDisplay */ uint8_t display_ : 1;
+  /* DurationStyle */ uint8_t style_ : 3;
+
+  auto display() const { return static_cast<DurationDisplay>(display_); }
+
+  auto style() const { return static_cast<DurationStyle>(style_); }
+};
+
+}  // namespace intl
+
 class DurationFormatObject : public NativeObject {
  public:
   static const JSClass class_;
@@ -38,7 +79,9 @@ class DurationFormatObject : public NativeObject {
   static constexpr uint32_t NUMBER_FORMAT_MICROSECONDS_SLOT = 9;
   static constexpr uint32_t NUMBER_FORMAT_NANOSECONDS_SLOT = 10;
   static constexpr uint32_t LIST_FORMAT_SLOT = 11;
-  static constexpr uint32_t SLOT_COUNT = 12;
+  static constexpr uint32_t OPTIONS_SLOT = 12;
+  static constexpr uint32_t TIME_SEPARATOR_SLOT = 13;
+  static constexpr uint32_t SLOT_COUNT = 14;
 
   static_assert(INTERNALS_SLOT == INTL_INTERNALS_OBJECT_SLOT,
                 "INTERNALS_SLOT must match self-hosting define for internals "
@@ -82,6 +125,30 @@ class DurationFormatObject : public NativeObject {
 
   void setListFormat(mozilla::intl::ListFormat* listFormat) {
     setFixedSlot(LIST_FORMAT_SLOT, PrivateValue(listFormat));
+  }
+
+  intl::DurationFormatOptions* getOptions() const {
+    const auto& slot = getFixedSlot(OPTIONS_SLOT);
+    if (slot.isUndefined()) {
+      return nullptr;
+    }
+    return static_cast<intl::DurationFormatOptions*>(slot.toPrivate());
+  }
+
+  void setOptions(intl::DurationFormatOptions* options) {
+    setFixedSlot(OPTIONS_SLOT, PrivateValue(options));
+  }
+
+  JSString* getTimeSeparator() const {
+    const auto& slot = getFixedSlot(TIME_SEPARATOR_SLOT);
+    if (slot.isUndefined()) {
+      return nullptr;
+    }
+    return slot.toString();
+  }
+
+  void setTimeSeparator(JSString* timeSeparator) {
+    setFixedSlot(TIME_SEPARATOR_SLOT, StringValue(timeSeparator));
   }
 
  private:
