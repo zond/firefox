a7dc004e1be7ba08b580b85e71e54f7d424f5366
Gabriel Luong <gabriel.luong@gmail.com>
2025-07-11 14:41:27 +0000

    Bug 1972159 - Part 20: Remove SessionControlView wallpaper onboarding and first frame handling r=android-reviewers,devota
    - The same logic is handled in https://searchfox.org/mozilla-central/rev/76f8c782e800003bcecf9da13815b8dc750e1668/mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt#995-1012 for the Compose Homepage.
    
    Differential Revision: https://phabricator.services.mozilla.com/D254593

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -565,7 +565,6 @@ class HomeFragment : Fragment() {
             binding.homepageView.isVisible = false
             binding.sessionControlRecyclerView.isVisible = true
             sessionControlView = SessionControlView(
-                containerView = binding.sessionControlRecyclerView,
                 interactor = sessionControlInteractor,
             )
 
