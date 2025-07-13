de4a4c2d7fbee98941b012b59f1871986cceabca
Gabriel Luong <gabriel.luong@gmail.com>
2025-07-13 02:28:40 +0000

    Bug 1972159 - Part 25: Enable Compose Homepage by default and remove associated settings and Nimbus flags r=android-reviewers,devota
    Differential Revision: https://phabricator.services.mozilla.com/D254598

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -551,11 +551,7 @@ class HomeFragment : Fragment() {
             listenForMicrosurveyMessage(requireContext())
         }
 
-        if (requireContext().settings().enableComposeHomepage) {
-            initComposeHomepage()
-        } else {
-            binding.homepageView.isVisible = false
-        }
+        initComposeHomepage()
 
         FxNimbus.features.homescreen.recordExposure()
 
@@ -905,8 +901,6 @@ class HomeFragment : Fragment() {
     }
 
     private fun initComposeHomepage() {
-        binding.homepageView.isVisible = true
-
         binding.homepageView.apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
 
