6f5f3700f9e49f80d86d99fe85f5f9717875a051
Andrej Glavic <aglavic@mozilla.com>
2025-07-15 15:06:28 +0000

    Bug 1827602 - Add screenshots to the rest of the startup tests. r=perftest-reviewers,sparky
    Differential Revision: https://phabricator.services.mozilla.com/D256524

--- testing/performance/mobile-startup/android_startup_cmff_cvns.py
+++ testing/performance/mobile-startup/android_startup_cmff_cvns.py
@@ -1,6 +1,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import os
 import re
 import sys
 import time
@@ -106,6 +107,7 @@ class Startup_test:
         measurements = {}
         # Iterate through the tests in the test list
         print(f"Running {self.test_name} on {self.package_id}...")
+        self.device.shell("mkdir -p /sdcard/Download")
         time.sleep(self.get_warmup_delay_seconds())
         self.skip_onboarding(self.test_name)
         test_measurements = []
@@ -119,7 +121,12 @@ class Startup_test:
             self.device.shell("logcat -c")
             process = self.device.shell_output(start_cmd_args).splitlines()
             test_measurements.append(self.get_measurement(self.test_name, process))
-
+            if i % 10 == 0:
+                screenshot_file = f"/sdcard/Download/{self.product}_iteration_{i}_startup_done_frame.png"
+                self.device.shell(f"screencap -p {screenshot_file}")
+                self.device.command_output(
+                    ["pull", "-a", screenshot_file, os.environ["TESTING_DIR"]]
+                )
         self.device.stop_application(self.package_id)
         print(f"{self.test_name}: {str(test_measurements)}")
         # Bug 1934023 - create way to pass median and still have replicates available
