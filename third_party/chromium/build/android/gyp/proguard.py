e1f008a09599e29fe60dd75dcda0ddbeab17167a
Michael Froman <mfroman@mozilla.com>
2025-07-16 21:10:40 +0300

    Bug 1974813 - vendor chromium/build from e30e6c0a6 r=ng?
    Differential Revision: https://phabricator.services.mozilla.com/D257346

--- third_party/chromium/build/android/gyp/proguard.py
+++ third_party/chromium/build/android/gyp/proguard.py
@@ -54,10 +54,6 @@ _IGNORE_WARNINGS = (
         # https://crbug.com/1441226
         r'PaymentRequest[BH]',
     ]) + ')',
-    # TODO(agrieve): Remove once we update to U SDK.
-    r'OnBackAnimationCallback',
-    # This class was added only in the U PrivacySandbox SDK: crbug.com/333713111
-    r'Missing class android.adservices.common.AdServicesOutcomeReceiver',
     # We enforce that this class is removed via -checkdiscard.
     r'FastServiceLoader\.class:.*Could not inline ServiceLoader\.load',
     # Happens on internal builds. It's a real failure, but happens in dead code.
@@ -80,9 +76,6 @@ _IGNORE_WARNINGS = (
     # androidx/test/espresso/web/internal/deps/guava/collect/Maps$1.class:"
     # Also happens in espresso core.
     r'Warning in .*:androidx/test/espresso/.*/guava/collect/.*',
-
-    # We are following up in b/290389974
-    r'AppSearchDocumentClassMap\.class:.*Could not inline ServiceLoader\.load',
 )
 
 _BLOCKLISTED_EXPECTATION_PATHS = [
@@ -103,8 +96,8 @@ def _ParseOptions():
                       help='Path to our custom R8 wrapper to use.')
   parser.add_argument('--input-paths',
                       action='append',
-                      required=True,
-                      help='GN-list of .jar files to optimize.')
+                      help='GN-list of .jar files to optimize, excluding'
+                      ' those --feature-jars.')
   parser.add_argument('--output-path', help='Path to the generated .jar file.')
   parser.add_argument('--tracerefs-json-out')
   parser.add_argument(
@@ -240,7 +233,6 @@ def _ParseOptions():
       options.sdk_extension_jars)
   options.proguard_configs = action_helpers.parse_gn_list(
       options.proguard_configs)
-  options.input_paths = action_helpers.parse_gn_list(options.input_paths)
   options.extra_mapping_output_paths = action_helpers.parse_gn_list(
       options.extra_mapping_output_paths)
   if os.environ.get('R8_VERBOSE') == '1':
@@ -256,6 +248,13 @@ def _ParseOptions():
     options.feature_jars = [
         action_helpers.parse_gn_list(x) for x in options.feature_jars
     ]
+    assert not options.input_paths
+    input_paths = set()
+    for jar_paths in options.feature_jars:
+      input_paths.update(jar_paths)
+    options.input_paths = sorted(input_paths)
+  else:
+    options.input_paths = action_helpers.parse_gn_list(options.input_paths)
 
   split_map = {}
   if options.uses_split:
@@ -331,14 +330,14 @@ def _OptimizeWithR8(options, config_paths, libraries, dynamic_config_data):
                                       parent_name=parent_name)
         split_contexts_by_name[name] = split_context
     else:
-      # Base context will get populated via "extra_jars" below.
       split_contexts_by_name['base'] = _SplitContext('base',
-                                                     options.output_path, [],
+                                                     options.output_path,
+                                                     options.input_paths,
                                                      tmp_output)
     base_context = split_contexts_by_name['base']
 
-    # R8 OOMs with xmx=2G.
-    cmd = build_utils.JavaCmd(xmx='3G') + [
+    # R8 OOMs with xmx=3G.
+    cmd = build_utils.JavaCmd(xmx='4G') + [
         # Allows -whyareyounotinlining, which we don't have by default, but
         # which is useful for one-off queries.
         '-Dcom.android.tools.r8.experimental.enablewhyareyounotinlining=1',
@@ -424,12 +423,6 @@ def _OptimizeWithR8(options, config_paths, libraries, dynamic_config_data):
           '--startup-profile',
           options.input_art_profile,
       ]
-
-    # Add any extra inputs to the base context (e.g. desugar runtime).
-    extra_jars = set(options.input_paths)
-    for split_context in split_contexts_by_name.values():
-      extra_jars -= split_context.input_jars
-    base_context.input_jars.update(extra_jars)
 
     for split_context in split_contexts_by_name.values():
       if split_context is base_context:
