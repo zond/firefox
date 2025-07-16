b63483d789127a7d0b369cf1c71bfc94590515ad
Sandor Molnar <smolnar@mozilla.com>
2025-07-16 19:15:43 +0300

    Revert "Bug 1977087 - fix two consecutive separator lines to tab menu r=Mardak,firefox-ai-ml-reviewers" for causing bc failures @ browser_chat_page.js
    This reverts commit 2d4030a451acdce8fb6b9dbfe7dd9e5b86cb71df.

--- browser/components/genai/GenAI.sys.mjs
+++ browser/components/genai/GenAI.sys.mjs
@@ -794,14 +794,7 @@ export const GenAI = {
     await this.buildAskChatMenu(menu, {
       browser,
       selectionInfo: null,
-      showItem: (item, shouldShow) => {
-        const separator = item.nextElementSibling;
-        this.showItem(item, shouldShow);
-
-        if (separator && separator.localName === "menuseparator") {
-          this.showItem(separator, shouldShow);
-        }
-      },
+      showItem: (item, shouldShow) => this.showItem(item, shouldShow),
       source: "tab",
       contextTabs,
     });
