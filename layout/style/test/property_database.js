0dd8cd9cc02218d2f90eb903554ddd6428e95def
Daniel Holbert <dholbert@cs.stanford.edu>
2025-07-17 14:25:44 +0000

    Bug 1977696: Remove about:config pref layout.css.transform-box-content-stroke.enabled. r=layout-reviewers,emilio
    This pref has been enabled-by-default since Firefox 125 in 2024.
    
    Differential Revision: https://phabricator.services.mozilla.com/D257594

--- layout/style/test/property_database.js
+++ layout/style/test/property_database.js
@@ -3843,7 +3843,7 @@ var gCSSProperties = {
     inherited: false,
     type: CSS_TYPE_LONGHAND,
     initial_values: ["view-box"],
-    other_values: ["fill-box", "border-box"],
+    other_values: ["fill-box", "border-box", "content-box", "stroke-box"],
     invalid_values: ["padding-box", "margin-box"],
   },
   "transform-origin": {
@@ -12124,15 +12124,6 @@ gCSSProperties.scale = {
     "10 calc(1 + 10px)",
   ],
 };
-
-if (
-  IsCSSPropertyPrefEnabled("layout.css.transform-box-content-stroke.enabled")
-) {
-  gCSSProperties["transform-box"]["other_values"].push(
-    "content-box",
-    "stroke-box"
-  );
-}
 
 gCSSProperties["touch-action"] = {
   domProp: "touchAction",
