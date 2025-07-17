0d15c1ad1f2f675a26c381e34d892969d0e29304
Kagami Sascha Rosylight <saschanaz@outlook.com>
2025-07-17 19:24:03 +0000

    Bug 1977489 - Hide MozAppearance from CSSStyleProperties r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D257411

--- layout/style/test/property_database.js
+++ layout/style/test/property_database.js
@@ -1736,7 +1736,8 @@ var gCSSProperties = {
     invalid_values: [],
   },
   "-moz-appearance": {
-    domProp: "MozAppearance",
+    // Hidden from CSSProperties, see bug 1977489.
+    // domProp: "MozAppearance",
     inherited: false,
     type: CSS_TYPE_SHORTHAND_AND_LONGHAND,
     alias_for: "appearance",
