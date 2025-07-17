a67a1bce7947160824ee8f48d4ddc09c967e210a
Nathan Barrett <nbarrett@mozilla.com>
2025-07-17 02:29:12 +0000

    Bug 197748 - Create lists widget with basic functionality r=home-newtab-reviewers,reemhamz
    Differential Revision: https://phabricator.services.mozilla.com/D257395

--- browser/extensions/newtab/bin/render-activity-stream-html.js
+++ browser/extensions/newtab/bin/render-activity-stream-html.js
@@ -126,6 +126,21 @@ function templateHTML(options) {
       type="module"
       src="chrome://global/content/elements/moz-radio-group.mjs"
     ></script>
+    <script
+      async
+      type="module"
+      src="chrome://global/content/elements/moz-select.mjs"
+    ></script>
+    <script
+      async
+      type="module"
+      src="chrome://global/content/elements/moz-option.mjs"
+    ></script>
+      <script
+      async
+      type="module"
+      src="chrome://global/content/elements/moz-reorderable-list.mjs"
+    ></script>
   </body>
 </html>
 `.trimLeft();
