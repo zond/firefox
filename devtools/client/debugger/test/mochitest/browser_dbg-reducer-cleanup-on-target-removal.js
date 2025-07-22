66e8617afe5e0973092c0be5ec4ac2781becb1ac
Alexandre Poirot <poirot.alex@gmail.com>
2025-07-22 15:01:30 +0000

    Bug 1971817 - [devtools] Use a dedicated action to remove sources and source actor objects. r=devtools-reviewers,bomsy
    This will help reuse this codepath when removing a pretty printed source when toggling it.
    Also tweak the existing cleanup code to be using similar filtering patterns and more thorough.
    
    We weren't clearing mutableOriginalSources completely in sources reducers,
    and there was a typo with sourceId instead of source.id in highlightedLineRange.
    
    Differential Revision: https://phabricator.services.mozilla.com/D255804

--- devtools/client/debugger/test/mochitest/browser_dbg-reducer-cleanup-on-target-removal.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-reducer-cleanup-on-target-removal.js
@@ -86,7 +86,7 @@ add_task(async function () {
   );
   let state = dbg.store.getState();
 
-  ok(!!Object.keys(state.ast.mutableInScopeLines).length, "Some scopes exists");
+  Assert.greater(state.ast.mutableInScopeLines.size, 0, "Some scopes exists");
   Assert.greater(
     state.sourceActors.mutableSourceActors.size,
     0,
