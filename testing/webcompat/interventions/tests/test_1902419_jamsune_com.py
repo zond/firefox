3d839e69e6ded68be4da6d72aa6ddb4b5b84dbd0
Thomas Wisniewski <twisniewski@mozilla.com>
2025-07-25 17:29:30 +0000

    Bug 1902419 - add a desktop-only JS webcompat intervention for www.jamsune.com to hide unsupported alert (also improve our alert-handling in intervention automated tests); r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D258659

--- testing/webcompat/interventions/tests/test_1902419_jamsune_com.py
+++ testing/webcompat/interventions/tests/test_1902419_jamsune_com.py
@@ -1,22 +1,20 @@
 import pytest
 
-URL = "https://sekowf.wns.com/sekoapin/#/login?authenticated=true&sso=true"
-
-UNSUPPORTED_ALERT = "chrome browser"
-LOGIN_CSS = "#logindiv"
+URL = "https://www.jamsune.com/"
+UNSUPPORTED_ALERT = "크롬으로만"
 
 
+@pytest.mark.skip_platforms("android")
 @pytest.mark.asyncio
 @pytest.mark.with_interventions
 async def test_enabled(client):
-    await client.navigate(URL, wait="none")
-    assert client.await_css(LOGIN_CSS, is_displayed=True)
+    await client.navigate(URL)
     assert not await client.find_alert(delay=3)
 
 
+@pytest.mark.skip_platforms("android")
 @pytest.mark.asyncio
 @pytest.mark.without_interventions
 async def test_disabled(client):
-    await client.make_preload_script("window.close = function() {}")
     await client.navigate(URL, wait="none")
     assert await client.await_alert(UNSUPPORTED_ALERT)
