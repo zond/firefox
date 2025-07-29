e630f40a64101bb7c3f85f602c6655ef4c1f50a6
alwu <alwu@mozilla.com>
2025-07-29 17:05:47 +0000

    Bug 1978434 - part1 : enhance MediaKeySession to correctly resolve closed promise based on the reason for closure. r=media-playback-reviewers,padenot
    As per the specification, the `close()` method resolves the closed promise with
    `closed-by-application` as the default reason [1]
    
    However, in the event of a CDM crash leading to its unavailability, the promise
    should be resolved with `internal-error` [2], thereby notifying the web
    application to refrain from creating any new session using the affected media
    key.
    
    [1] https://w3c.github.io/encrypted-media/#ref-for-dom-mediakeysessionclosedreason-closed-by-application-3
    [2] https://w3c.github.io/encrypted-media/#ref-for-dom-mediakeysessionclosedreason-internal-error-2
    
    Differential Revision: https://phabricator.services.mozilla.com/D258790

--- dom/media/eme/mediafoundation/WMFCDMProxyCallback.cpp
+++ dom/media/eme/mediafoundation/WMFCDMProxyCallback.cpp
@@ -74,12 +74,14 @@ void WMFCDMProxyCallback::OnSessionKeyExpiration(
 }
 
 void WMFCDMProxyCallback::OnSessionClosed(const nsString& aSessionId) {
-  NS_DispatchToMainThread(
-      NS_NewRunnableFunction("WMFCDMProxyCallback::OnSessionClosed",
-                             [self = RefPtr{this}, this, aSessionId]() {
-                               RETURN_IF_NULL(mProxy);
-                               mProxy->OnSessionClosed(aSessionId);
-                             }));
+  NS_DispatchToMainThread(NS_NewRunnableFunction(
+      "WMFCDMProxyCallback::OnSessionClosed",
+      [self = RefPtr{this}, this, aSessionId]() {
+        RETURN_IF_NULL(mProxy);
+        mProxy->OnSessionClosed(
+            aSessionId,
+            dom::MediaKeySessionClosedReason::Closed_by_application);
+      }));
 }
 
 void WMFCDMProxyCallback::Shutdown() {
