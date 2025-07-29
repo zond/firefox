1df3f563d05d31f04335133bedf2aad4de662d99
Tanvi Manku <tanvi.manku@gmail.com>
2025-07-29 03:07:27 +0000

    Bug 1979592 - Show toolbar in null search and render notifications in correct position. r=mtigley
    Differential Revision: https://phabricator.services.mozilla.com/D258974

--- toolkit/components/satchel/megalist/content/MegalistAlpha.mjs
+++ toolkit/components/satchel/megalist/content/MegalistAlpha.mjs
@@ -407,10 +407,6 @@ export class MegalistAlpha extends MozLitElement {
   renderList() {
     return this.records.length
       ? html`
-          <div class="first-row">
-            ${this.renderSearch()} ${this.renderMenu()}
-          </div>
-          <div class="second-row">${this.renderRadioButtons()}</div>
           <div
             class="passwords-list"
             role="listbox"
@@ -784,6 +780,15 @@ export class MegalistAlpha extends MozLitElement {
     `;
   }
 
+  renderToolbar() {
+    return html`
+      <div class="first-row">${this.renderSearch()} ${this.renderMenu()}</div>
+      ${this.header
+        ? html` <div class="second-row">${this.renderRadioButtons()}</div> `
+        : ""}
+    `;
+  }
+
   async #scrollPasswordCardIntoView(guid) {
     const matchingRecordIndex = this.records.findIndex(
       record => record.origin.guid === guid
@@ -817,6 +822,10 @@ export class MegalistAlpha extends MozLitElement {
   }
 
   render() {
+    const showToolbar =
+      this.viewMode === VIEW_MODES.ALERTS ||
+      (this.viewMode === VIEW_MODES.LIST && this.header?.value?.total > 0);
+
     return html`
       <link
         rel="stylesheet"
@@ -828,6 +837,7 @@ export class MegalistAlpha extends MozLitElement {
           data-l10n-attrs="heading"
           view="viewCPMSidebar"
         ></sidebar-panel-header>
+        ${when(showToolbar, () => html` ${this.renderToolbar()} `)}
         ${this.renderNotification()} ${this.renderContent()}
       </div>
     `;
