0230d7bc50588e6cb55103a8397aaf43546fdb80
Andrea Marchesini <amarchesini@mozilla.com>
2025-08-06 10:16:32 +0000

    Bug 1981281 - Update the CookieStore.set name/value validation algorithm, r=valentin
    Differential Revision: https://phabricator.services.mozilla.com/D260066

--- dom/cookiestore/CookieStore.cpp
+++ dom/cookiestore/CookieStore.cpp
@@ -54,9 +54,6 @@ int32_t SameSiteToConst(const CookieSameSite& aSameSite) {
 }
 
 bool ValidateCookieNameOrValue(const nsAString& aStr) {
-  if (aStr.Length() > 0 && (aStr.First() == 0x20 || aStr.Last() == 0x20)) {
-    return false;
-  }
   for (auto iter = aStr.BeginReading(), end = aStr.EndReading(); iter < end;
        ++iter) {
     if (*iter == 0x3B || *iter == 0x7F || (*iter <= 0x1F && *iter != 0x09)) {
@@ -64,6 +61,31 @@ bool ValidateCookieNameOrValue(const nsAString& aStr) {
     }
   }
   return true;
+}
+
+const nsDependentSubstring TrimTabAndSpace(const nsAString& aStr) {
+  nsAString::const_iterator start, end;
+
+  aStr.BeginReading(start);
+  aStr.EndReading(end);
+
+  auto TabOrSpace = [](char16_t ch) { return ch == 0x09 || ch == 0x20; };
+
+  while (start != end && TabOrSpace(*start)) {
+    ++start;
+  }
+
+  while (end != start) {
+    --end;
+
+    if (!TabOrSpace(*end)) {
+      ++end;
+
+      break;
+    }
+  }
+
+  return Substring(start, end);
 }
 
 bool ValidateCookieNameAndValue(const nsAString& aName, const nsAString& aValue,
@@ -77,6 +99,11 @@ bool ValidateCookieNameAndValue(const nsAString& aName, const nsAString& aValue,
 
   if (!ValidateCookieNameOrValue(aValue)) {
     aPromise->MaybeRejectWithTypeError("Cookie value contains invalid chars");
+    return false;
+  }
+
+  if (aName.Contains('=')) {
+    aPromise->MaybeRejectWithTypeError("Cookie name cannot contain '='");
     return false;
   }
 
@@ -389,14 +416,16 @@ already_AddRefed<Promise> CookieStore::Set(const CookieInit& aOptions,
   NS_DispatchToCurrentThread(NS_NewRunnableFunction(
       __func__, [self = RefPtr(this), promise = RefPtr(promise), aOptions,
                  cookiePrincipal = RefPtr(cookiePrincipal.get())]() {
-        if (!ValidateCookieNameAndValue(aOptions.mName, aOptions.mValue,
-                                        promise)) {
+        nsString name(TrimTabAndSpace(aOptions.mName));
+        nsString value(TrimTabAndSpace(aOptions.mValue));
+
+        if (!ValidateCookieNameAndValue(name, value, promise)) {
           return;
         }
 
         nsString domain;
-        if (!ValidateCookieDomain(cookiePrincipal, aOptions.mName,
-                                  aOptions.mDomain, domain, promise)) {
+        if (!ValidateCookieDomain(cookiePrincipal, name, aOptions.mDomain,
+                                  domain, promise)) {
           return;
         }
 
@@ -404,8 +433,8 @@ already_AddRefed<Promise> CookieStore::Set(const CookieInit& aOptions,
           return;
         }
 
-        if (!ValidateCookieNamePrefix(aOptions.mName, aOptions.mValue, domain,
-                                      aOptions.mPath, promise)) {
+        if (!ValidateCookieNamePrefix(name, value, domain, aOptions.mPath,
+                                      promise)) {
           return;
         }
 
@@ -446,7 +475,7 @@ already_AddRefed<Promise> CookieStore::Set(const CookieInit& aOptions,
                 mozilla::WrapNotNull(cookieURI.get()),
                 cookiePrincipal->OriginAttributesRef(), thirdPartyContext,
                 partitionForeign, usingStorageAccess, isOn3PCBExceptionList,
-                aOptions.mName, aOptions.mValue,
+                name, value,
                 // If expires is not set, it's a session cookie.
                 aOptions.mExpires.IsNull(), ComputeExpiry(aOptions), domain,
                 aOptions.mPath, SameSiteToConst(aOptions.mSameSite),
