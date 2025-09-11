7a97f1f324149f2147cd73de12de04cd14e35354
Martin Bruse <zondolfin@gmail.com>
2025-09-11 08:51:22 +0000

    Bug 1986393 - Vendored rust. r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D263387

--- python/mozbuild/mozbuild/vendor/vendor_rust.py
+++ python/mozbuild/mozbuild/vendor/vendor_rust.py
@@ -340,6 +340,8 @@ Please commit or stash these changes before vendoring, or re-run with `--ignore-
         "BSD-3-Clause": [
             "subtle",
             "uritemplate-next",
+            "jxl",
+            "jxl_macros",
         ],
     }
 
